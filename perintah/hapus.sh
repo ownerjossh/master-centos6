c40f40e6a22df9cfe0f26c2c12a7a4b53af78cda
ownerjossh <sutris10mei1996@gmail.com>
2017-04-22 14:42:17 +0700

    hapus.sh

---
+++ perintah/hapus.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Script Hapus User SSH
+read -p "Username yang akan dihapus : " Login
+userdel $Login
+echo -e ".................................................."
+echo -e "User \e[1;33;44m$Login\e[0m Berhasil dihapus"
