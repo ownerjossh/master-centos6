014884ec754b54b7993ca73c6214575d701819da
ownerjossh <sutris10mei1996@gmail.com>
2017-05-15 16:15:02 +0700

    Update and rename test5.sh to test6.sh

--- perintah/test6.sh
+++ perintah/test6.sh
@@ -5,8 +5,7 @@ echo "------------------------------- BANNED AKUN SSH --------------------------
 echo "                            ALL SUPPORTED BY JoSSH                               "
 echo "                       https://facebook.com/www.sutriez                          "
 echo "                                 087802844588                                    "
-echo -e "                        Script Installer by \e[1;21;40mJoSSH TEAM\e[0m        "
-echo """""                       
+echo -e "                        Script Installer by \e[1;21;40mJoSSH TEAM\e[0m        "                      
 
 # begin of user-list
 echo "-------------------------------"
